d11c9c9ec09491527932c18fa10dd22c9a0d78b4
Ruslan Tumasov <Rus-nura@yandex.ru>
2020-09-05 17:12:05 +0700

    Added getAverageTest() test

--- src/test/java/DeviationTest.java
+++ src/test/java/DeviationTest.java
@@ -5,9 +5,16 @@ import statistics.utils.DoubleUtils;
 
 public class DeviationTest {
     @Test
+    public void averageTest() {
+        double[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        double average = Deviation.getAverage(values);
+        Assert.assertEquals(5.5, DoubleUtils.round(average, 1), 0);
+    }
+
+    @Test
     public void standardDeviationTest() {
         double[] values = {1, 5, 2, 7, 1, 9, 3, 8, 5, 9};
         double standardDeviation = Deviation.getStandardDeviation(values, 1);
-        Assert.assertEquals(3.16, DoubleUtils.round(standardDeviation, 2), 0.0);
+        Assert.assertEquals(3.16, DoubleUtils.round(standardDeviation, 2), 0);
     }
 }
